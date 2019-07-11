a7ba424b525634e31eab1f7f484d83cb5c87658d
Daniell Mesquita <daniellplasmmer@gmail.com>
2019-07-11 00:58:40 -0400

    Install: creates layers and dna folder, creates small floflis executable that points to installed floflis DNA repo

--- install.sh
+++ install.sh
@@ -14,8 +14,17 @@ cat << "EOF"
  |___| |_||_| /__/  \__| \__,_| |_| |_| \___| |_|  
 
 EOF
-echo "- Installing Floflis' DNA in /usr/sbin..."
-sudo cp floflis /usr/sbin
-echo "- Turning Floflis DNA into a executable..."
+echo "- Installing Floflis in /usr/sbin..."
+sudo cat > /usr/sbin/floflis << ENDOFFILE
+#!/bin/bash
+source /usr/sbin/layers/dna/floflis
+ENDOFFILE
+echo "- Creating Floflis' Layers folder..."
+sudo mkdir /usr/sbin/layers
+echo "- Creating folder for Floflis' DNA in /usr/sbin/layers..."
+sudo mkdir /usr/sbin/layers/dna
+echo "- Installing Floflis' DNA..."
+sudo cp -r . /usr/sbin/layers/dna
+echo "- Turning Floflis into a executable..."
 sudo chmod 755 /usr/sbin/floflis
 echo "(✓) Successfully installed. Just type 'floflis' (without quotes) and hit 'enter' button to open it."
