f13f9aa4629eda1d88ecfe5d3228098af4579aac
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-01 20:20:29 -0300

    fix modules.sh!

--- modules.sh
+++ modules.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # information-reader
-if [ -e /usr/lib/floflis/layers/dna/modules/information-reader.sh ]
+if [ -f /usr/lib/floflis/layers/dna/modules/information-reader.sh ]
 then
-    . /usr/lib/floflis/layers/dna/modules/information-reader.sh
+    . /usr/lib/floflis/layers/dna/modules/./information-reader.sh
 fi
 # energy
-if [ -e /usr/lib/floflis/layers/dna/modules/energy.sh ]
+if [ -f /usr/lib/floflis/layers/dna/modules/energy.sh ]
 then
-    . /usr/lib/floflis/layers/dna/modules/energy.sh
+    . /usr/lib/floflis/layers/dna/modules/./energy.sh
 fi
