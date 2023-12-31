87448ec5a2125a688231e736f452ae431c661aa9
Daniella Mesquita <mesquitadaniella@proton.me>
2023-12-31 19:50:32 -0300

    modules.sh: export variables to modules

--- modules.sh
+++ modules.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export FLOPREFIX
+export flofmach && export flofdistro && export flofarch && export osfullname && export osname && export osversion && export osbuild && export osbuildcodename && export updatepatch && export year && export layer && export nxtlayer && export distrobase && export user && export specialbuildattempt
+
 # information-reader
 if [ -f "$FLOPREFIX"usr/lib/floflis/layers/dna/modules/information-reader.sh ]
 then
